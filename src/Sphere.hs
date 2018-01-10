446bed2f62f3fdeb2c82417f3befea3faea2e081
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2018-01-10 12:21:39 +0100

    sphere is a shape now

--- src/Sphere.hs
+++ src/Sphere.hs
@@ -1,5 +1,6 @@
 module Sphere where
 
+import Ray
 import Shape
 import Vector
 
@@ -7,4 +8,10 @@ type Center = Vector
 type Radius = Double
 
 sphere::Center -> Radius -> Shape
-sphere center rad = undefined
+sphere center r =
+-- type Shape = Env -> Ray -> Maybe (Distance, Colour)
+  \env (Ray start dir) -> 
+    let v = sub start center in
+    let discr = (dot v dir ^^ 2) - len2 v +  r ^^ 2 in
+    let dist = (-(dot v dir)) - sqrt discr in
+    if discr < 0 then Nothing else Just (dist, (Colour 1.0 1.0 1.0))
