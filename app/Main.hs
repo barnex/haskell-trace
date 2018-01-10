e0bddcd5905c6d667964ebdfebbd58f714bf1cbf
Kasper Janssens <kasper.janssens@wdc.com>
2018-01-10 12:53:47 +0000

    wrong use of dollar

--- app/Main.hs
+++ app/Main.hs
@@ -67,14 +67,13 @@ width = 800
 height :: Int
 height = 600
       
-
 main :: IO ()
 main =
   let c = Vector 0.0 0.0 (-1.0) in
   let r = 0.5 in
-  let s = paint $ sphere c r $ white in
+  let s = paint (sphere c r) $ white in
   let c' = Vector 1.0 0.0 (-2.0) in
-  let s' = paint $ sphere c' r $ white in
+  let s' = paint (sphere c' r) white in
   let ray = Ray (Vector 1.0 2.0 0.0) (Vector 0.0 (-1.0) 0.0) in
   let env = Env{ scene = [s, s'] , backgroundColour = Colour 0.0 0.0 0.0 } in
   do
