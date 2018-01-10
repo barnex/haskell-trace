eda86ed3e33d0944cf5f102c7805114025a7cf4b
Kasper Janssens <kasper.janssens@wdc.com>
2018-01-10 12:41:01 +0000

    Nothing Tabs, Just Spaces

--- src/Types.hs
+++ src/Types.hs
@@ -4,6 +4,6 @@ data Colour = Colour Double Double Double
 
 srgb::Double -> Double
 srgb x =
-	if x <= 0.0031308 then 12.92 * x else 1.055*(x ** (1.0 / 2.4)) - 0.05
+  if x <= 0.0031308 then 12.92 * x else 1.055*(x ** (1.0 / 2.4)) - 0.05
 
 type Distance = Double
