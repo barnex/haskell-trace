705ad091dd609b5c0ac8e298e85b85ede2fa4e09
Kasper <kasper.janssens.android@gmail.com>
2018-02-22 12:10:59 +0000

    Make tes knock like a bus

--- test/MaterialSpec.hs
+++ test/MaterialSpec.hs
@@ -10,4 +10,4 @@ spec =
     it "is not knocking like a bus" $
       let dir' = normalize $ vector 0.0 1.0 2.0 in
       let normal = normalize $ vector 1.0 1.0 1.0 in
-      reflected dir' normal `shouldBe` normalize (vector 5.0 6.0 7.0)
+      reflected dir' normal `shouldBe` Unit (-0.8944271909999159, -0.44721359549995804,-2.2204460492503126e-16)
