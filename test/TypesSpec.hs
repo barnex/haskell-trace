06f8bd5ed408e7cee529e1fda281047bfca527e1
Kasper <kasper.janssens.android@gmail.com>
2018-02-22 12:04:36 +0000

    Fix tests

--- test/TypesSpec.hs
+++ test/TypesSpec.hs
@@ -10,9 +10,9 @@ spec :: Spec
 spec = 
   describe "intersection of sphere and ray" $ do
     it "should be 1.0 for Vector 1 2 and Sphere (0 1) 1" $
-      let c = Vector 0.0 1.0 0.0 in
+      let c = vector 0.0 1.0 0.0 in
       let r = 1 in
       let s = sphere c r in
-      let ray = Ray (Vector 1.0 2.0 0.0) (Vector 0.0 (-1.0) 0.0) in
+      let ray = Ray (vector 1.0 2.0 0.0) (normalize $ vector 0.0 (-1.0) 0.0) in
       do
         Just 1.0  `shouldBe` Just 1.0
